08a69479bff2e9db2da091627aba1c79026f50da
tkhk-gh <tkhk-gh@users.noreply.github.com>
2018-12-11 20:42:58 +0900

    Complete day2 part2

--- day2/go/main.go
+++ day2/go/main.go
@@ -8,11 +8,11 @@ import (
 )
 
 func main() {
-	fmt.Println(part1())
+	fmt.Println(part2())
 }
 
 func part1() int {
-	f, err := os.Open(`input.txt`)
+	f, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,7 +28,6 @@ func part1() int {
 		for _, b := range s.Bytes() {
 			letterCounter[b]++
 		}
-
 		twoLetterCounted := false
 		threeLetterCounted := false
 		for _, v := range letterCounter {
@@ -43,4 +42,47 @@ func part1() int {
 	}
 
 	return twoLetterCount * threeLetterCount
+}
+
+func part2() string {
+	f, err := os.Open("input.txt.org")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// あらかじめ input を配列にする
+	s := bufio.NewScanner(f)
+
+	inputData := [][]byte{}
+	for s.Scan() {
+		copySrc := s.Bytes()
+		copyDest := make([]byte, len(copySrc), cap(copySrc))
+		copy(copyDest, copySrc)
+		inputData = append(inputData, copyDest)
+	}
+
+	var result []byte
+	for i, src := range inputData {
+		for _, target := range inputData[i+1:] {
+			tmp := dripMatchByte(src, target)
+			if len(result) < len(tmp) {
+				result = tmp
+			}
+		}
+	}
+	return string(result)
+}
+
+func dripMatchByte(l, r []byte) []byte {
+	if len(l) != len(r) {
+		log.Fatalf("len(l) != len(r): len(%d) != len(%d)", len(l), len(r))
+	}
+	var result []byte
+	for i := 0; i < len(l); i++ {
+		if l[i] == r[i] {
+			result = append(result, l[i])
+		}
+	}
+	return result
 }
